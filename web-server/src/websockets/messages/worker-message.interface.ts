650bd6080e502a21877c26953a222deddc879f93
Dmitry <dmitryporotov@gmail.com>
2024-04-16 17:15:52 +0300

    added fields to WorkerMessageInterface

--- web-server/src/websockets/messages/worker-message.interface.ts
+++ web-server/src/websockets/messages/worker-message.interface.ts
@@ -1,5 +1,11 @@
 export interface WorkerMessageInterface {
     gameId: string
     userId?: number
-    type: "action"
+    type: "action",
+    action: string,
+    status?: {created: boolean, details: unknown},
+    reply?: Array<{
+        to: '*' | number,
+        player_action: unknown
+    }>
 }
