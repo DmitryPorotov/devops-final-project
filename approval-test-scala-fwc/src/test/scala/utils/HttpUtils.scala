deea028dc7a7295a7425c50019499b801eca11e4
Dmitry <dmitryporotov@gmail.com>
2024-07-31 19:01:54 +0300

    changed url to be the new url

--- approval-test-scala-fwc/src/test/scala/utils/HttpUtils.scala
+++ approval-test-scala-fwc/src/test/scala/utils/HttpUtils.scala
@@ -8,14 +8,14 @@ object HttpUtils {
 
   def response(email: String, password: String = "12345678"): Response[String] =
     quickRequest
-      .post(uri"http://localhost:8888/api/v1/auth/login")
+      .post(uri"http://localhost:8888/fwc/api/v1/auth/login")
       .contentType("application/json")
       .body(s"{\"email\":\"$email\",\"password\":\"$password\"}")
       .send()
   
   def login(email: String, password: String = "12345678"): UserDTO =
     val resp = quickRequest
-      .post(uri"http://localhost:8888/api/v1/auth/login")
+      .post(uri"http://localhost:8888/fwc/api/v1/auth/login")
       .contentType("application/json")
       .body(s"{\"email\":\"$email\",\"password\":\"$password\"}")
       .send()
