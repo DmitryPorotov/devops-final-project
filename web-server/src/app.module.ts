c3192c69346feef746e46acd1ab74fd306e22bbb
Dmitry <dmitryporotov@gmail.com>
2024-08-19 15:22:57 +0300

    added a way to get game state through http

--- web-server/src/app.module.ts
+++ web-server/src/app.module.ts
@@ -8,12 +8,15 @@ import {JwtModule} from "@nestjs/jwt";
 import {EventsModule} from "./websockets/events.module";
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './redis/redis.module';
+import {AdminModule} from "./admin/admin.module";
+import {GamesModule} from "./admin/games/games.module";
+
 
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), UserModule, AuthModule, LobbyModule, JwtModule, EventsModule, RedisModule],
+  }), UserModule, AuthModule, LobbyModule, JwtModule, EventsModule, RedisModule, AdminModule, GamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
