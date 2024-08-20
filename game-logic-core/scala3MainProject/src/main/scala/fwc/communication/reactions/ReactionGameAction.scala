ec1940ad6f5f0f3ac3f39f9f3970193167492827
Dmitry <dmitryporotov@gmail.com>
2024-08-20 15:43:38 +0300

    bug fix: added SubPhaseRecalculateSupplies to the match

--- game-logic-core/scala3MainProject/src/main/scala/fwc/communication/reactions/ReactionGameAction.scala
+++ game-logic-core/scala3MainProject/src/main/scala/fwc/communication/reactions/ReactionGameAction.scala
@@ -127,6 +127,7 @@ object ReactionGameAction {
       case _: SubPhaseCollectTaxes => ActionCollectTaxes(gameState)
       case s: SubPhaseDisableOrder => ActionDisableOrder(gameState, s.orderType)
       case _: SubPhaseCleanUpAfterRound => ActionCleanUpAfterRound(gameState, isRandom)
+      case _: SubPhaseRecalculateSupplies => ActionRecalculateSupplies(gameState)
 //      case _ => throw new RuntimeException("SubPhase " + subPhase + " has no matching action.")
 
 
