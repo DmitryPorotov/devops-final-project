f6f9fef7d2d4546126326486afc1fd986cbee8e1
Dmitry <dmitryporotov@gmail.com>
2024-08-20 17:39:40 +0300

    bug fix: added option to not remove opponent card with lion 5 card

--- game-logic-core/scala3MainProject/src/main/scala/fwc/gameSaving/actions/action/ActionResolveCardLion5.scala
+++ game-logic-core/scala3MainProject/src/main/scala/fwc/gameSaving/actions/action/ActionResolveCardLion5.scala
@@ -9,7 +9,8 @@ import ujson.Value
 
 case class ActionResolveCardLion5(
                                    gameState: GameState,
-                                   houseType: HouseType
+                                   houseType: HouseType,
+                                   doCancelCard: Boolean,
                                  ) extends Action(gameState) with PlayerAction(houseType) with JsonSerializable {
   override def doAction(): GameState = {
     val (isAttackerAction, updatedCombat, _) = CardResolveBeforeCombat.validateAndGetCombatAndCardPhase(
@@ -18,7 +19,14 @@ case class ActionResolveCardLion5(
       gameState.combat,
       gameState.powerTokens(HouseType.Kraken)
     )
-
+    
+    if !doCancelCard 
+    then return gameState.copy(
+      subPhase = SubPhaseGetTidesOfBattleCards(Seq(gameState.combat.attackerHouse, gameState.combat.defenderHouse)),
+      combat = updatedCombat
+    )
+      
+    
     val opponentHouseType =
       if isAttackerAction
       then gameState.combat.defenderHouse
@@ -45,13 +53,14 @@ case class ActionResolveCardLion5(
     )
   }
 
-  def hasCardsLeft(houseType: HouseType): Boolean = {
+  private def hasCardsLeft(houseType: HouseType): Boolean = {
     gameState.discardedHouseCards.getOrElse(houseType, Seq()).size < 6
   }
 
   override def toJson: Value = ujson.Obj(
     Action.actionTypeJsonKey -> "resolveCardLion5",
-    "houseType" -> ujson.Str(houseType.toString)
+    "houseType" -> ujson.Str(houseType.toString),
+    "doCancelCard" -> doCancelCard
   )
 }
 
@@ -59,6 +68,7 @@ object ActionResolveCardLion5 extends JsonParsableAction {
   override def fromJson(gameState: GameState, json: Value): ActionResolveCardLion5 =
     ActionResolveCardLion5(
       gameState,
-      HouseType.fromString(json("houseType").str)
+      HouseType.fromString(json("houseType").str),
+      json("doCancelCard").bool
     )
 }
