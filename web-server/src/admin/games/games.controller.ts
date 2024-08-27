8c93e0b97b5433adea399a39f678739097653fda
Dmitry <dmitryporotov@gmail.com>
2024-08-27 17:54:10 +0300

    added listing saves and loading games to the games admin http

--- web-server/src/admin/games/games.controller.ts
+++ web-server/src/admin/games/games.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Req, UseGuards} from '@nestjs/common';
 import {Roles} from "../../auth/roles.decorator";
 import {AuthGuard} from "../../auth/auth.guard";
 import {GamesService} from "./games.service";
+import {Request} from "express";
 
 @Roles('admin')
 @UseGuards(AuthGuard)
@@ -10,11 +11,19 @@ export class GamesController {
     constructor(private readonly gamesService: GamesService) {
     }
     @Get(':id')
-    async state(@Param('id') id: string) {
-        return await this.gamesService.getState(id);
+    async state(@Param('id') id: string, @Req() request: Request) {
+        return await this.gamesService.getState(id, request['user']);
     }
     @Post(':id')
-    async save(@Param('id') id: string, @Body() {name}: {name: string}) {
-        return await this.gamesService.saveGame(id, name);
+    async save(@Param('id') id: string, @Body() {name}: {name: string}, @Req() request: Request) {
+        return await this.gamesService.saveGame(id, name, request['user']);
+    }
+    @Get(':userId/saves')
+    async listSaves(@Param('userId') userId: string, @Req() request: Request) {
+        return await this.gamesService.listSaves(+userId, request['user']);
+    }
+    @Post(':lobbyId/load')
+    async loadGame(@Param('lobbyId') lobbyId: string, @Body() {name}: {name: string}, @Req() request: Request) {
+        return await this.gamesService.loadGame(lobbyId, name, request['user']);
     }
 }
--- web-server/src/admin/games/games.service.ts
+++ web-server/src/admin/games/games.service.ts
@@ -1,38 +1,59 @@
 import { Injectable } from '@nestjs/common';
 import RedisPubSub from "../../redis/redis.pub-sub";
+import {randomUUID} from 'crypto'
+import {LoginUserDto} from "../../user/dto/login-user.dto";
 
 @Injectable()
 export class GamesService {
-    async getState(id: string): Promise<any> {
+    async getState(id: string, adminUser: LoginUserDto): Promise<object> {
         return this.sendToWorker({
             gameId: id,
             userId: -1,
-            messageId: "" + Math.random(),
+            messageId: randomUUID(),
             action: 'get_game_state',
-        });
+        }, adminUser.id);
     }
 
-    async saveGame(id: string, name: string): Promise<any> {
+    async saveGame(id: string, name: string, adminUser: LoginUserDto): Promise<void> {
         this.sendToWorker({
             gameId: id,
             userId: -1,
-            messageId: "" + Math.random(),
+            messageId: randomUUID(),
             action: 'save',
             saveName: name,
-        })
+        }, adminUser.id)
     }
 
-    private async sendToWorker(json: Object): Promise<any> {
+    async listSaves(userId: number, adminUser: LoginUserDto): Promise<object> {
+        return this.sendToWorker({
+            gameId: "3",
+            userId: userId,
+            messageId: randomUUID(),
+            action: 'list_saves',
+        }, adminUser.id)
+    }
+
+    async loadGame(lobbyId: string, name: string, adminUser: LoginUserDto): Promise<void> {
+        return this.sendToWorker({
+            gameId: lobbyId,
+            userId: -1,
+            messageId: randomUUID(),
+            action: 'load',
+            saveName: name
+        }, adminUser.id)
+    }
+
+    private async sendToWorker(json: Object, adminId: number): Promise<any> {
         const redisPubSub = new RedisPubSub();
         await redisPubSub.init(()=>{},()=>{});
         return new Promise(async (resolve, reject) => {
             try {
-                await redisPubSub.subscribe(`admin.*`, (message: string, channel: string) => {
+                await redisPubSub.subscribe(`admin${adminId}.*`, (message: string, channel: string) => {
                     resolve(message);
-                    redisPubSub.unsubscribe(`admin.*`)
+                    redisPubSub.unsubscribe(`admin${adminId}.*`)
                 });
 
-                await redisPubSub.publishToGame('worker1' /*todo*/, 'admin', JSON.stringify(json))
+                await redisPubSub.publishToGame('worker1' /*todo*/, `admin${adminId}`, JSON.stringify(json))
             }
             catch (e) {
                 reject(e)
