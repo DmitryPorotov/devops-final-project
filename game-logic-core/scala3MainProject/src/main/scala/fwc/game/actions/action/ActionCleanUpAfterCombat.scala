6f4bfb6a165d0ba192b2f2e17702de49666b8149
Dmitry <dmitryporotov@gmail.com>
2024-09-04 17:47:26 +0300

    bug fix and refactor, updated game state is sent to functions now

--- game-logic-core/scala3MainProject/src/main/scala/fwc/game/actions/action/ActionCleanUpAfterCombat.scala
+++ game-logic-core/scala3MainProject/src/main/scala/fwc/game/actions/action/ActionCleanUpAfterCombat.scala
@@ -1,12 +1,13 @@
 package fwc.game.actions.action
 
 import fwc.JsonSerializable
-import fwc.game.actions.{Action, ActionException, JsonParsableAction}
+import fwc.game.actionPhase.{Combat, DiscardedHouseCards}
+import fwc.game.actions.{Action, JsonParsableAction}
 import fwc.game.{GameState, gameRules}
-import fwc.game.board.{MilitaryUnit, MilitaryUnitType, TileNumber}
+import fwc.game.board.{Armies, MilitaryUnit, MilitaryUnitType, TileNumber}
 import fwc.game.houses.HouseType
 import fwc.game.phases.actionSubPhases.{SubPhaseCalculateGameWinner, SubPhaseResolveHouseCard}
-import fwc.game.planningPhase.OrderType
+import fwc.game.planningPhase.{OrderType, PlacedOrders}
 import ujson.Value
 
 import scala.collection.immutable.Seq
@@ -18,62 +19,80 @@ case class ActionCleanUpAfterCombat(
   override def doAction(): GameState = {
 
     val combat = gameState.combat
-    val updatedArmies =
-      if combat.loserCard.exists(_.isPufferfish2) && combat.loser.contains(combat.defenderHouse)
-      then gameState.armies + (combat.attackerTileNum ->
-        (gameState.armies.getOrElse(combat.attackerTileNum, Seq()) ++ combat.attackerArmy)
-      )
-      else {
-        if combat.winner.contains(combat.attackerHouse)
-        then 
-          if combat.attackerArmy.nonEmpty
-          then gameState.armies + (combat.defenderTileNum -> combat.attackerArmy)
-          else gameState.armies - combat.defenderTileNum
-        else 
-          if combat.defenderArmy.nonEmpty
-          then gameState.armies + (combat.defenderTileNum -> combat.defenderArmy)
-          else gameState.armies - combat.defenderTileNum
-      }
+    val updatedArmies = `move winner's army to enbattled tile` (combat)
 
-    val updatedDiscardedCards =
-      gameState.discardedHouseCards.concat(Map(
-        combat.attackerHouse -> (
-          if combat.attackerCard == null then gameState.discardedHouseCards(combat.attackerHouse)
-          else gameState.discardedHouseCards(combat.attackerHouse) :+ combat.attackerCard.code
-          ),
-        combat.defenderHouse -> (
-          if combat.defenderCard == null then gameState.discardedHouseCards(combat.defenderHouse)
-          else gameState.discardedHouseCards(combat.defenderHouse) :+ combat.defenderCard.code
+    val updatedDiscardedCards = `discard cards used in combat` (combat)
+
+    val updatedDiscardedCards2 = `restore wolf's cards if card 2 was used` (combat, updatedDiscardedCards)
+
+    val updatedPlacedOrders = `move march order to embattled tile if rose won with card 0` (combat)
+
+    val updatedPlacedOrders2 = `remove defender's order if he lost` (combat, updatedPlacedOrders)
+
+    val updatedTokens = `give winner 2 power tokens if he had lion 2 card` (combat, updatedArmies)
+
+    val numOfCastles = `count winner's castles` (combat, updatedArmies)
+
+    val updatedGameState_ArmiesPlacedOrders = gameState.copy(
+      armies = updatedArmies,
+      placedOrders = updatedPlacedOrders2
+    )
+
+    val newPhase = `calculate new sub phase` (combat, numOfCastles, updatedGameState_ArmiesPlacedOrders, updatedArmies)
+
+    updatedGameState_ArmiesPlacedOrders.copy(
+      subPhase = newPhase,
+      combat = if newPhase.isInstanceOf[SubPhaseResolveHouseCard] then combat else null,
+      discardedHouseCards = 
+        if newPhase.isInstanceOf[SubPhaseResolveHouseCard] 
+        then updatedDiscardedCards2
+        else updatedDiscardedCards2.resetDecksAfterCombat(combat),
+      powerTokens = updatedTokens
+    )
+  }
+
+  private def `calculate new sub phase` (combat: Combat, numOfCastles: TileNumber, updatedGameState_ArmiesPlacedOrders: GameState, updatedArmies: Armies) = {
+    if numOfCastles >= 7
+    then SubPhaseCalculateGameWinner(HouseType.getSeqOfAll)
+    else if combat.winnerCard.exists(_.isMoose2)
+      && {
+      val footmenExistsFunc =
+        (army: Seq[MilitaryUnit], support: Seq[TileNumber]) =>
+          army.exists(_.unitType == MilitaryUnitType.Footmen)
+            || support.foldLeft(false)(
+            (acc, tn) =>
+              updatedGameState_ArmiesPlacedOrders.armies(tn).foldLeft(false)(
+                (acc2, mu: MilitaryUnit) =>
+                  acc2 || (mu.unitType == MilitaryUnitType.Footmen && mu.house == HouseType.Moose)
+              ) || acc
           )
-      ))
 
-    val updatedDiscardedCards2 =
-      if combat.loserCard.exists(_.isWolf2)
-      then updatedDiscardedCards - HouseType.Wolf
-      else updatedDiscardedCards
+      val hasAvailableMooseKnightsFunc =
+        () => {
+          val c = updatedArmies.foldLeft[TileNumber](0)(
+            (acc: TileNumber, tileNumberArmy: (TileNumber, Seq[MilitaryUnit])) =>
+              acc + tileNumberArmy._2.count(mu => mu.unitType == MilitaryUnitType.Knights && mu.house == HouseType.Moose)
+          )
+          c < gameRules.maxArmies(MilitaryUnitType.Knights)
+        }
 
-    val updatedPlacedOrders =
-      if combat.winnerCard.exists(_.isRose0) && combat.winner.contains(combat.attackerHouse)
-      then gameState.placedOrders.placeOrder(
-        combat.attackerHouse,
-        combat.defenderTileNum,
-        combat.attackerOrder,
-        0
-      )
-      else gameState.placedOrders
-
-    val updatedPlacedOrders2 =
       if combat.winner.contains(combat.attackerHouse)
-      then updatedPlacedOrders.removeOrder(combat.defenderHouse, combat.defenderTileNum)
-      else updatedPlacedOrders
+      then footmenExistsFunc(combat.attackerArmy, combat.attackerSupport)
+        && hasAvailableMooseKnightsFunc()
+      else footmenExistsFunc(combat.defenderArmy, combat.defenderSupport)
+        && hasAvailableMooseKnightsFunc()
+    }
+    then SubPhaseResolveHouseCard(HouseType.Moose, 2) // todo looks like there is an infinite loop here (jvm crashes)
+    else if combat.winnerCard.exists(_.isLion1)
+    then SubPhaseResolveHouseCard(HouseType.Lion, 1)
+    else if combat.winnerCard.exists(_.isMoose3) || combat.loserCard.exists(_.isMoose3)
+    then SubPhaseResolveHouseCard(HouseType.Moose, 3)
+    else NextOrderFinder.nextSubPhase(updatedGameState_ArmiesPlacedOrders, OrderType.March, combat.attackerHouse)
+  }
 
-    val updatedTokens =
-      if combat.winnerCard.exists(_.isLion2)
-      then gameState.powerTokens.addTokens(HouseType.Lion, 2, updatedArmies)
-      else gameState.powerTokens
-
-    val numOfCastles = updatedArmies.foldLeft(0)(
-      (acc: Int, cur: (TileNumber, Seq[MilitaryUnit])) =>
+  private def `count winner's castles`(combat: Combat, updatedArmies: Armies) = {
+    updatedArmies.foldLeft(0)(
+      (acc: TileNumber, cur: (TileNumber, Seq[MilitaryUnit])) =>
         val tile = gameRules.board(cur._1)
         if tile.musteringPoints > 0
           && cur._2.exists(_.house == combat.winner.head)
@@ -82,65 +101,69 @@ case class ActionCleanUpAfterCombat(
     ) + {
       if Try[Seq[MilitaryUnit]] {
         updatedArmies(gameRules.board.find(_.homeOf == combat.winner.head).head.number)
-      } .getOrElse(Seq()).isEmpty
+      }.getOrElse(Seq()).isEmpty
       then 1
       else 0
     }
+  }
 
-    val newPhase =
-      if numOfCastles >= 7
-      then SubPhaseCalculateGameWinner(HouseType.getSeqOfAll)
-      else
-      if combat.winnerCard.exists(_.isMoose2)
-        && {
-        val footmenExistsFunc =
-          (army: Seq[MilitaryUnit], support: Seq[Int]) =>
-            army.exists(_.unitType == MilitaryUnitType.Footmen)
-              || support.foldLeft(false)(
-              (acc, tn) =>
-                gameState.armies(tn).foldLeft(false)(
-                  (acc2, mu: MilitaryUnit) =>
-                    acc2 || (mu.unitType == MilitaryUnitType.Footmen && mu.house == HouseType.Moose)
-                ) || acc
-            )
+  private def `give winner 2 power tokens if he had lion 2 card`(combat: Combat, updatedArmies: Armies) = {
+    if combat.winnerCard.exists(_.isLion2)
+    then gameState.powerTokens.addTokens(HouseType.Lion, 2, updatedArmies)
+    else gameState.powerTokens
+  }
 
-        val hasAvailableMooseKnightsFunc =
-          () => {
-            val c = updatedArmies.foldLeft[Int](0)(
-              (acc: Int, tileNumberArmy: (TileNumber, Seq[MilitaryUnit])) =>
-                acc + tileNumberArmy._2.count(mu => mu.unitType == MilitaryUnitType.Knights && mu.house == HouseType.Moose)
-            )
-            c < gameRules.maxArmies(MilitaryUnitType.Knights)
-          }
+  private def `remove defender's order if he lost`(combat: Combat, updatedPlacedOrders: PlacedOrders) = {
+    if combat.winner.contains(combat.attackerHouse)
+    then updatedPlacedOrders.removeOrder(combat.defenderHouse, combat.defenderTileNum)
+    else updatedPlacedOrders
+  }
 
-        if combat.winner.contains(combat.attackerHouse)
-        then footmenExistsFunc(combat.attackerArmy, combat.attackerSupport)
-          && hasAvailableMooseKnightsFunc()
-        else footmenExistsFunc(combat.defenderArmy, combat.defenderSupport)
-          && hasAvailableMooseKnightsFunc()
-      }
-      then SubPhaseResolveHouseCard(HouseType.Moose, 2) // todo looks like there is an infinite loop here (jvm crashes)
-      else if combat.winnerCard.exists(_.isLion1)
-      then SubPhaseResolveHouseCard(HouseType.Lion, 1)
-      else if (combat.winnerCard.exists(_.isMoose3) || combat.loserCard.exists(_.isMoose3))
-        && {
-          val loserDiscardedCards: Seq[Int] = gameState.discardedHouseCards.getOrElse(combat.loser.head, Seq())
-          loserDiscardedCards.size < 7
-        }
-      then SubPhaseResolveHouseCard(HouseType.Moose, 3)
-      else NextOrderFinder.nextSubPhase(gameState, OrderType.March, combat.attackerHouse)
-
-    gameState.copy(
-      subPhase = newPhase,
-      armies = updatedArmies,
-      combat = if newPhase.isInstanceOf[SubPhaseResolveHouseCard] then combat else null,
-      discardedHouseCards = 
-        if newPhase.isInstanceOf[SubPhaseResolveHouseCard] 
-        then updatedDiscardedCards2
-        else updatedDiscardedCards2.resetDecksAfterCombat(combat),
-      placedOrders = updatedPlacedOrders2,
-      powerTokens = updatedTokens
+  private def `move march order to embattled tile if rose won with card 0`(combat: Combat) = {
+    if combat.winnerCard.exists(_.isRose0) && combat.winner.contains(combat.attackerHouse)
+    then gameState.placedOrders.placeOrder(
+      combat.attackerHouse,
+      combat.defenderTileNum,
+      combat.attackerOrder,
+      0
     )
+    else gameState.placedOrders
+  }
+
+  private def `restore wolf's cards if card 2 was used`(combat: Combat, updatedDiscardedCards: DiscardedHouseCards) = {
+    if combat.loserCard.exists(_.isWolf2)
+    then updatedDiscardedCards - HouseType.Wolf
+    else updatedDiscardedCards
+  }
+
+  private def `discard cards used in combat`(combat: Combat) = {
+    gameState.discardedHouseCards.concat(Map(
+      combat.attackerHouse -> (
+        if combat.attackerCard == null then gameState.discardedHouseCards(combat.attackerHouse)
+        else gameState.discardedHouseCards(combat.attackerHouse) :+ combat.attackerCard.code
+        ),
+      combat.defenderHouse -> (
+        if combat.defenderCard == null then gameState.discardedHouseCards(combat.defenderHouse)
+        else gameState.discardedHouseCards(combat.defenderHouse) :+ combat.defenderCard.code
+        )
+    ))
+  }
+
+  private def `move winner's army to enbattled tile`(combat: Combat) = {
+    if combat.loserCard.exists(_.isPufferfish2) && combat.loser.contains(combat.defenderHouse)
+    then gameState.armies + (combat.attackerTileNum ->
+      (gameState.armies.getOrElse(combat.attackerTileNum, Seq()) ++ combat.attackerArmy)
+      )
+    else {
+      if combat.winner.contains(combat.attackerHouse)
+      then
+        if combat.attackerArmy.nonEmpty
+        then gameState.armies + (combat.defenderTileNum -> combat.attackerArmy)
+        else gameState.armies - combat.defenderTileNum
+      else if combat.defenderArmy.nonEmpty
+      then gameState.armies + (combat.defenderTileNum -> combat.defenderArmy)
+      else gameState.armies - combat.defenderTileNum
+    }
   }
 
   override def toJson: Value = ujson.Obj(
