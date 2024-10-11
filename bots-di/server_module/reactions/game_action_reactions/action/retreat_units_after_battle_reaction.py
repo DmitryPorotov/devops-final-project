ca7ba07487174042afe944b95cd1d19e9d405f45
Dmitry <dmitryporotov@gmail.com>
2024-10-11 17:24:39 +0300

    fixed retreat action so units who can't retreat don't

--- bots-di/server_module/reactions/game_action_reactions/action/retreat_units_after_battle_reaction.py
+++ bots-di/server_module/reactions/game_action_reactions/action/retreat_units_after_battle_reaction.py
@@ -1,6 +1,8 @@
 from DTO.actions.action import  ActionRetreatUnitsAfterBattle
 from DTO.messages.reply import Reply
 from server_module.game_state.game_state import GameState
+from server_module.game_state.military_unit import MilitaryUnit
+from server_module.game_state.military_unit_type import MilitaryUnitType
 from server_module.reactions.game_action_reactions.base_action_reaction import BaseActionReaction
 
 
@@ -13,5 +15,12 @@ class RetreatUnitsAfterBattleReaction(BaseActionReaction):
         target_tn = str(pa['targetTileNumber'])
         if target_tn not in self._game_state.armies:
             self._game_state.armies[target_tn] = []
-        self._game_state.armies[target_tn].extend(self._game_state.combat.defender_army)
+        self._game_state.armies[target_tn].extend(list(MilitaryUnit(
+            u.house,
+            u.unit_type,
+            True
+        ) for u in self._game_state.combat.defender_army if u.unit_type is not MilitaryUnitType.GARRISON
+                                                       and u.unit_type is not MilitaryUnitType.SIEGE_ENGINES
+                                                       and u.unit_type is not MilitaryUnitType.POWER_TOKEN
+                                                       and not u.is_defeated))
         self.logger.info(pa)
