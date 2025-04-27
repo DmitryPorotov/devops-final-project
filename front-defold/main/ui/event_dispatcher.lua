82ce6c4e9af471ad7caaf101edf418f1483cd479
Dmitry <dmitryporotov@gmail.com>
2025-04-27 12:07:37 +0300

    made event_dispatcher work with multiple handlers

--- front-defold/main/ui/event_dispatcher.lua
+++ front-defold/main/ui/event_dispatcher.lua
@@ -1,31 +1,41 @@
 local _M = {}
 
 local subscriptions = {}
--- note: do I need multiple handlers for the same event?
 
 --- Subscribe to an event
 ---@param event string Event name
 ---@param callback function Event handler
 ---@param this self Optional self to be passed to the callback
 function _M.on(event, callback, this)
-	subscriptions[event] = { c = callback, t = this }
+	if not subscriptions[event] then
+		subscriptions[event] = {}
+	end
+	subscriptions[event][callback] = { t = this }
 end
 
 --- Unsubscribe from event
 ---@param event string Event name
 ---@param callback function UNUSED. A callback used to subscribe to this event. Will be used if I switch to multiple handlers for the same event
 function _M.off(event, callback)
-	subscriptions[event] = nil
+	if not subscriptions[event] then
+		return
+	end
+	subscriptions[event][callback] = nil
+	if not next(subscriptions[event]) then
+		subscriptions[event] = nil
+	end
 end
 
 --- Trigger an event
 ---@param event string Event name
 function _M.trigger(event, ...)
 	if subscriptions[event] then
-		if subscriptions[event].t then
-			subscriptions[event].c(subscriptions[event].t, ...)
-		else
-			subscriptions[event].c(...)
+		for k, v in pairs(subscriptions[event]) do
+			if v.t then
+				k(v.t, ...)
+			else
+				k(...)
+			end
 		end
 	end
 end
